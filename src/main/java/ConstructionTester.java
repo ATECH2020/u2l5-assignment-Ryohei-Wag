caf31df6064e4d721399b6821b3e427c8400cf38
Ryohei Wagley <ryohei.2040988@nv.ccsd.net>
2020-09-22 23:48:03 +0000

    ConstructionTester HowFarAway

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -4,6 +4,26 @@ public class ConstructionTester
 {
     public static void main(String[] args)
     {
-        // Start here!
+      //declare
+     double tax;
+     int boardNum, windowNum;
+       /*
+       ask users these: sales tax, number of boards, and number of windows.
+       then construct the Construction class to calculate the total cost, which is
+       addition of cost of lumbers(boards) and windows. each one can be calculate
+       by using lumberCost and windowCost methods in the Construction class. To find
+       grand total cost, use grandTotal method in the Construction class.
+       */
+       Scanner in  = new Scanner(System.in);
+       System.out.println("Enter the sales tax rate:");
+       tax = in.nextDouble();
+       System.out.println("How many boards do you need?");
+       boardNum = in.nextInt();
+       System.out.println("How many windows do you need?");
+       windowNum = in.nextInt();
+       Construction constr = new Construction(8, 11, tax);
+       double total = constr.lumberCost(boardNum) + constr.windowCost(windowNum);
+       System.out.println("Total: " + total);
+       System.out.println("Grand Total: " + constr.grandTotal(total));
     }
 }
--- src/main/java/HowFarAway.java
+++ src/main/java/HowFarAway.java
@@ -4,6 +4,24 @@ public class HowFarAway
 {
     public static void main(String[] args)
     {
-        // your code here.
+       //declare
+     double startLat, startLog, endLat, endLog;
+      /*
+         User enter the four values:
+         beginning value of latitude and longitude, and ending value of latitude and
+         longitude.
+         Then construct a GeoLocation class to be able to use a calculation method.
+      */
+       Scanner in = new Scanner(System.in);
+       System.out.print("Enter the latitude of the starting location: ");
+       startLat = in.nextDouble();
+       System.out.print("Enter the longitude of the starting location: ");
+       startLog = in.nextDouble();
+       System.out.print("Enter the latitude of the ending location: ");
+       endLat = in.nextDouble();
+       System.out.print("Enter the longitude of the ending location: ");
+       endLog = in.nextDouble();
+       GeoLocation strLocation = new GeoLocation(startLat, startLog);
+       System.out.println("The distance is " + strLocation.distanceFrom(new GeoLocation(endLat, endLog)) + " miles.");
     }
 }
